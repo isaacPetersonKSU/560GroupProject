193efa79955ee9516fc6557c879e223d92a7f00e
Mrkoelzer <69817133+Mrkoelzer@users.noreply.github.com>
2021-11-03 13:04:03 -0500

    test

--- GroupProject/GroupProject/Form1.Designer.cs
+++ GroupProject/GroupProject/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace GroupProject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 553);
+            this.ClientSize = new System.Drawing.Size(970, 573);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
