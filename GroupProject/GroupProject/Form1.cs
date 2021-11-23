b838528e4c87222ec27522e962622a7b261eff8b
Jacob Koelzer <mrkoelzer@ksu.edu>
2021-11-23 16:24:07 -0600

    Datagridview added

--- GroupProject/GroupProject/Form1.cs
+++ GroupProject/GroupProject/Form1.cs
@@ -7,14 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace GroupProject
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PrimaryData;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False");
+        SqlDataAdapter adpt;
+        DataTable dt;
         public Form1()
         {
             InitializeComponent();
+            showdata();
+        }
+
+        public void showdata()
+        {
+            adpt = new SqlDataAdapter("select * from Season.Player", con);
+            dt = new DataTable();
+            adpt.Fill(dt);
+            dataGridView1.DataSource = dt;
         }
     }
 }
