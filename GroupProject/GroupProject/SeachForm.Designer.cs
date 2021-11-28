e646dc98be1f68b0ac47651674a47a6ea89ded94
Isaac Peterson <ijp@ksu.edu>
2021-11-28 02:21:27 -0600

    played with the gui a little. its not good yet tho

--- GroupProject/GroupProject/SeachForm.Designer.cs
+++ GroupProject/GroupProject/SeachForm.Designer.cs
@@ -38,7 +38,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 115);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(776, 323);
             this.dataGridView1.TabIndex = 0;
--- GroupProject/GroupProject/TouchdownLeaders.Designer.cs
+++ GroupProject/GroupProject/TouchdownLeaders.Designer.cs
@@ -35,23 +35,27 @@ namespace GroupProject
             this.uxRecivingTDRadButt = new System.Windows.Forms.RadioButton();
             this.uxRushingTDRadButt = new System.Windows.Forms.RadioButton();
             this.uxTotalTDRadButt = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(172, 12);
+            this.dataGridView1.Location = new System.Drawing.Point(172, 57);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 82;
-            this.dataGridView1.Size = new System.Drawing.Size(802, 450);
+            this.dataGridView1.Size = new System.Drawing.Size(802, 432);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseClick);
             // 
             // uxSortLabel
             // 
             this.uxSortLabel.AutoSize = true;
-            this.uxSortLabel.Location = new System.Drawing.Point(11, 24);
+            this.uxSortLabel.Location = new System.Drawing.Point(9, 75);
             this.uxSortLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.uxSortLabel.Name = "uxSortLabel";
             this.uxSortLabel.Size = new System.Drawing.Size(50, 13);
@@ -61,7 +65,7 @@ namespace GroupProject
             // uxPassingTDRadButt
             // 
             this.uxPassingTDRadButt.AutoSize = true;
-            this.uxPassingTDRadButt.Location = new System.Drawing.Point(14, 54);
+            this.uxPassingTDRadButt.Location = new System.Drawing.Point(12, 105);
             this.uxPassingTDRadButt.Name = "uxPassingTDRadButt";
             this.uxPassingTDRadButt.Size = new System.Drawing.Size(127, 17);
             this.uxPassingTDRadButt.TabIndex = 3;
@@ -73,7 +77,7 @@ namespace GroupProject
             // uxRecivingTDRadButt
             // 
             this.uxRecivingTDRadButt.AutoSize = true;
-            this.uxRecivingTDRadButt.Location = new System.Drawing.Point(14, 77);
+            this.uxRecivingTDRadButt.Location = new System.Drawing.Point(12, 128);
             this.uxRecivingTDRadButt.Name = "uxRecivingTDRadButt";
             this.uxRecivingTDRadButt.Size = new System.Drawing.Size(138, 17);
             this.uxRecivingTDRadButt.TabIndex = 4;
@@ -85,7 +89,7 @@ namespace GroupProject
             // uxRushingTDRadButt
             // 
             this.uxRushingTDRadButt.AutoSize = true;
-            this.uxRushingTDRadButt.Location = new System.Drawing.Point(14, 100);
+            this.uxRushingTDRadButt.Location = new System.Drawing.Point(12, 151);
             this.uxRushingTDRadButt.Name = "uxRushingTDRadButt";
             this.uxRushingTDRadButt.Size = new System.Drawing.Size(129, 17);
             this.uxRushingTDRadButt.TabIndex = 5;
@@ -97,7 +101,7 @@ namespace GroupProject
             // uxTotalTDRadButt
             // 
             this.uxTotalTDRadButt.AutoSize = true;
-            this.uxTotalTDRadButt.Location = new System.Drawing.Point(14, 123);
+            this.uxTotalTDRadButt.Location = new System.Drawing.Point(12, 174);
             this.uxTotalTDRadButt.Name = "uxTotalTDRadButt";
             this.uxTotalTDRadButt.Size = new System.Drawing.Size(101, 17);
             this.uxTotalTDRadButt.TabIndex = 6;
@@ -106,18 +110,57 @@ namespace GroupProject
             this.uxTotalTDRadButt.UseVisualStyleBackColor = true;
             this.uxTotalTDRadButt.CheckedChanged += new System.EventHandler(this.uxRadButtSelectionChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(899, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(818, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(414, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(398, 20);
+            this.textBox1.TabIndex = 10;
+            // 
             // TouchdownLeaders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(986, 501);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.uxTotalTDRadButt);
             this.Controls.Add(this.uxRushingTDRadButt);
             this.Controls.Add(this.uxRecivingTDRadButt);
             this.Controls.Add(this.uxPassingTDRadButt);
             this.Controls.Add(this.uxSortLabel);
             this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "TouchdownLeaders";
             this.Text = "NFL Players";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -134,6 +177,10 @@ namespace GroupProject
         private System.Windows.Forms.RadioButton uxRecivingTDRadButt;
         private System.Windows.Forms.RadioButton uxRushingTDRadButt;
         private System.Windows.Forms.RadioButton uxTotalTDRadButt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
